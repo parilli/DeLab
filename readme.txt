532874670fd8706dd9ea438b2faaa4a0a88624c9
parilli <magaparilli@gmail.com>
2021-12-12 18:54:59 -0400

    Se agrego el archivo __init__.py con la primera funcion implentada (listOrganizations) y el archivo readme.txtcon el explicativo del producto

---
+++ readme.txt
@@ -0,0 +1,16 @@
+****************** Automatización de procesos en DeLab ******************
+
+En este repositorio se desarrollaron ciertas funciones para automatizar algunos procesos dentro de la compañia,
+y de esta manera mejorar la experiencia de nuestros empleados.
+
+Pasos a seguir:
+
+
+1. Deberá clonar el repositorio localmente, para poder disponer de las nuevas funciones.
+2. Una vez clonado el repositorio en su máquina local, para poder acceder a las funciones deberá importar el directorio utils
+Con el comando "import utils", podrá usar las funciones definidas en el archivo __init__.py.
+3. Cada vez que se use una de estas funciones deberá colocarse el prefijo utils. Ejm: "utils.listOrganizations"
+
+Las funciones implementadas hasta ahora son:
+
+    1. listOrganizations() ------>  sirve para listar todas las organizaciones que se tiene acceso con la API key
---
+++ utils/__init__.py
@@ -0,0 +1,35 @@
+import requests
+import json
+
+
+
+def listOrganizations():
+
+    # Primero hacemos la operación de getOrganizations 
+
+    url = "https://api.meraki.com/api/v1/organizations"    # dirección para hacer el request
+    headers = {
+    "Content-Type": "application/json",
+    "Accept": "application/json",
+    "X-Cisco-Meraki-API-Key": "6bec40cf957de430a6f1f2baa056b99a4fac9ea0"
+    }
+    payload = None
+    response = requests.get( url, headers=headers, data = payload)
+
+    # Luego pasamos la repuesta del request a json, para poder manejarla más fácil
+
+    organizationsInfo = json.loads(response.text)  # este objeto es de tipo lista
+    n = len(organizationsInfo)                     # el largo de esta lista es igual al número de organizaciones (en este caso 29)
+    print("Lista de organizaciones:")
+
+    # tenemos una lista que tiene por elementos objetos de tipo diccionario, cada diccionario contiene información
+    # específica de cada una de las organizaciones como el nombre, id, etc.
+    # Así que necesitamos acceder a cada uno de estos diccionarios uno a uno
+    
+    for i in range (n):
+        organizationName = organizationsInfo[i]['name']  # en el elemento i de la lista, accedemos a la categoría de 'name'
+        print(str(i)+"."+str(organizationName))
+
+   
+    
+
